207ceb0ec8d5e5c04cacb56837e5399ac0f37668
赫稲 <131579504+akaina0809@users.noreply.github.com>
2023-09-10 15:28:49 +0900

    Update count.js

--- js/count.js
+++ js/count.js
@@ -6,7 +6,7 @@ function count(){
 }
 function count_2(){
   var thisCount = $("#count_2").html();
-      thisCount = Number(50);
-      thisCount = 50 + 1;
+      thisCount = Number(thisCount);
+      thisCount = thisCount + 1;
   $("#count_2").html(thisCount);
 }
