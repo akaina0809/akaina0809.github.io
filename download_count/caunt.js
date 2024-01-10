fae5dc408f3e7d43bafb6ecb960696edc746d742
赫稲 <131579504+akaina0809@users.noreply.github.com>
2024-01-10 15:54:14 +0900

    Update caunt.js

--- download_count/caunt.js
+++ download_count/caunt.js
@@ -1,1 +1,24 @@
+window.onload =
+    function() {
+  let clickDiv = document.getElementById('click-div');
+  clickDiv.onclick = incrementClick;
 
+  let resetBtn = document.getElementById('reset-button');
+  resetBtn.onclick = resetCounter;
+}
+
+var counterVal = 0;
+
+incrementClick =
+    function() {
+  updateDisplay(++counterVal);
+}
+
+function resetCounter() {
+  counterVal = 0;
+  updateDisplay(counterVal);
+}
+
+function updateDisplay(val) {
+  document.getElementById('counter-label').innerHTML = val;
+}
