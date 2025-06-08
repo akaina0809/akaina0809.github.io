610fc4e4cbadc620739f29cdd567098b2ec34cee
赫稲 <131579504+akaina0809@users.noreply.github.com>
2025-06-09 07:18:59 +0900

    Add files via upload

---
+++ dl/z_addon_list.js
@@ -0,0 +1,31 @@
+let adlist = {
+  addon_list: [
+    {
+      name: "Touhou Komeiji Koishi GUI",
+      description: "東方プロジェクトのこいしのGUIです。",
+      type: ["リソースパック"],
+      junban: 1,
+      icon_url: "https://akaina0809.github.io/addon/touhou/images/Used.jpg?raw=true",
+      link: "https://akaina0809.github.io/addon/touhou/Komeiji_Koishi.html",
+      id: "release-info2"
+    },
+    {
+      name: "Skin change addon",
+      description: "スキンを変更できるアドオンです。",
+      type: ["ビヘイビアーパック"],
+      junban: 2,
+      icon_url: "https://akaina0809.github.io/addon/skin/english/png_1.png?raw=true",
+      link: "https://akaina0809.github.io/addon/skin/skin.html",
+      id: "release-info3"
+    },
+    {
+      name: "クリスマス:Christmas site",
+      description: "クリスマスプレゼント(2023年)",
+      type: ["リソースパック"],
+      junban: 3,
+      icon_url: "",
+      link: "https://akaina0809.github.io/addon/scoop/pack.html",
+      id: ""
+    },
+  ]
+}
