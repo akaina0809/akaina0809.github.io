93c861a368ffc896d86fa4feedb4377b4ae41d8d
赫稲 <131579504+akaina0809@users.noreply.github.com>
2025-06-09 14:16:27 +0900

    Update count.js

--- js/count.js
+++ js/count.js
@@ -1,6 +1,6 @@
 const owner = 'akaina0809'; // GitHubリポジトリの所有者名
 const repo = 'dsito';       // GitHubリポジトリの名前
-const githubToken = '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'; // 実際は絶対に公開しないでください！
+const githubToken = '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'; // 実際は絶対に公開しないでください！
 
 const fileNames = [
   'Touhou_Komeiji_Koishi_GUI_V1.5.mcpack',
